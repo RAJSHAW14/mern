cea696864735170019c95c42b65fb4262f27f669
RAJSHAW14 <39998628+RAJSHAW14@users.noreply.github.com>
2021-05-31 15:28:56 +0530

    db, env update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 import express from "express";
 import fs from 'fs';
+const morgan = require("morgan");
+import cors from "cors";
+import mongoose from "mongoose";
+
+require("dotenv").config();
 
 const app = express();
 
+//db connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser : true,
+    useCreateIndex : true,
+    useFindAndModify : false,
+    useUnifiedTopology : true,
+})
+.then(() => console.log('Database connected sucessfully'))
+.catch((err) => console.log('Databse not connectes',err));
+
+
 // router middleware
+app.use(cors());
+app.use(morgan("dev"));
 
 fs.readdirSync("./routes").map((r) => app.use("/api",require(`./routes/${r}`)));
 
